357c49153417feb73f326ff7f6c3968a8bf87394
davisnygren <davisnygren@gmail.com>
2015-03-24 14:05:55 -0400

    Added test case for 1

---
+++ src/PrimeFactors.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class PrimeFactors {
+
+	public static List<Integer> generate(int i) {
+		return new ArrayList<Integer>();
+	}
+	
+}
---
+++ src/TestPrimeFactors.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class TestPrimeFactors {
+
+	@Test
+	public void testOne() {
+		assertEquals(list(), PrimeFactors.generate(1));
+		}
+	
+	private List<Integer> list() {
+		return Arrays.asList();
+	}
+
+}
